40f421d51d2172fd000296240c054f329c113c95
sivanxpeer <sivan.x.peer@gmail.com>
2021-12-24 14:08:20 +0200

    connecting both buttons to data

--- src/components/CounterButton/CounterButton.js
+++ src/components/CounterButton/CounterButton.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react'
 import { data } from '../Api';
 // import Home from '../Home/Home.js';
 export class CounterButton extends Component {
-    state = { likes: 0, imageURL: "", index: 0, };
+    state = { likes: 0, imageURL: "", index: 0, description: "",};
 
-    changeImage = (i) => {
+    changeImageAndDesc = (i) => {
         // let i = this.state.index+1;
         if (data[this.state.index+1]) {
+            const item = data.find((itm)=>data[i]===itm)
+            // this.setState({ index: i });
+            // console.log(this.state);
+            console.log(item)
+            return item;
 
-            this.setState({ index: i });
-            console.log(this.state)
             // return (<img src={data[this.state.index].imageURL} alt={""} style={{ width: "200px", height: "200px" }}></img>);
-            return <img src={`${data[this.state.index + 1].imageURL}`} alt={""} style={{ width: "200px", height: "200px" }}></img>
+            // return <img src={`${data[this.state.index + 1].imageURL}`} alt={""} style={{ width: "200px", height: "200px" }}></img>
         }
     }
 
@@ -20,15 +23,14 @@ export class CounterButton extends Component {
         // let home = new Home();
         const stateTemp = {};
         stateTemp.index = this.state.index + 1;
-        if (data[this.state.index]) {
+        if (data[stateTemp.index+1]) {
             stateTemp.likes = this.state.likes + 1;
             stateTemp.imageURL = data[this.state.index].imageURL;
             console.log(stateTemp.likes, stateTemp.imageURL);
             this.setState({ likes: stateTemp.likes, imageURL: stateTemp.imageURL, index: stateTemp.index });
-            this.changeImage(stateTemp.index);
-            // home.getDataImage();
+            const img=this.changeImageAndDesc(stateTemp.index);
             return (<div>
-                {this.stateTemp}
+                <img src={img.imageURL}></img>
             </div>
             )
         }
