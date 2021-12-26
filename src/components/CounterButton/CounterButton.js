077a5d91253a88624d1b202b9f9d8491817e513b
sivanxpeer <sivan.x.peer@gmail.com>
2021-12-26 20:53:10 +0200

    changing images

--- src/components/CounterButton/CounterButton.js
+++ src/components/CounterButton/CounterButton.js
@@ -1,38 +1,15 @@
 import React, { Component } from 'react'
-import { data } from '../Api';
 // import Home from '../Home/Home.js';
 export class CounterButton extends Component {
-    state = { likes: 0, imageURL: "", index: 0, description: "woman", };
-
-    handleLike = () => {
-        const stateTemp = {...this.state};
-        stateTemp.index = this.state.index;
-        console.log(stateTemp);
-        if (data[stateTemp.index + 1]) {
-            stateTemp.index = this.state.index + 1;
-            stateTemp.likes = this.state.likes + 1;
-            stateTemp.description=data[stateTemp.index].description;
-            stateTemp.imageURL = data[stateTemp.index].imageURL;
-            console.log(stateTemp.imageURL);
-            this.setState({...stateTemp})
-            return (
-                <div>
-                    {this.state}
-                </div>
-            )
-        }
-        // else return;
-    }
-
-
     render() {
 
         return (
             <div>
                 <div className="count-likes">
-                    {this.state.likes}
+                    {this.props.numOfLikes}
+                    {this.props.numOfDislikes}
                 </div>
-                <button onClick={this.handleLike}>{this.props.buttonname}</button>
+                <button onClick={this.props.clickHandler}>{this.props.buttonname}</button>
             </div>
         )
     }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,44 +4,53 @@ import CounterButton from '../CounterButton/CounterButton';
 import './Home.css';
 
 export class Home extends Component {
-    printData = (item) => {
-        // console.log(data);
-        data.forEach(() => {
-            this.getDataImage();
-            this.getDataDescription();
-        })
-    }
+    state = { likes: 0, dislikes: 0, index: 0, isActive: false };
 
-    getDataImage = () => {
-        const obj = {};
-        obj.index = 0;
-        obj.imageURL = data[obj.index].imageURL;
-        return (<img src={obj.imageURL} alt={""} style={{ width: "200px", height: "200px" }}></img>);
-    }
-    getDataDescription = () => {
-        const obj = {};
-        obj.index = 0;
-        obj.description = data[obj.index].description;
-        return (obj.description);
-    }
+    handleLike = (counter) => {
+        
+    this.setState((prevState) => ({
+            [counter]: prevState[counter] += 1,
+            index: prevState.index += 1
+        }));
+        return [counter];
+    };
+
+    getDataImage = (i) => {
+        const imageURL = data[i].imageURL;
+        console.log(imageURL);
+        return (<img src={imageURL} alt={""} style={{ width: "200px", height: "200px" }}></img>);
+    };
+
+    getDataDescription = (i) => {
+        const description = data[i].description;
+        return (<div>{description}</div>);
+    };
 
     render() {
         return (
             <div className="Home">
-                {this.printData()}
                 <div className="main-container">
                     <div className="image">
-                        <img func={this.props.handleLike} src="" alt={""}>
+                        <img src="" alt={""}>
                         </img >
-                            {this.getDataImage()}
-                        {this.printData()}
+                        {this.getDataImage(this.state.index)}
                     </div>
                     <div className="description">
-                        {this.getDataDescription()}
+                        {this.getDataDescription(this.state.index)}
                     </div>
                     <div className="buttons">
-                        <CounterButton buttonname="Like"></CounterButton>
-                        <CounterButton buttonname="Unlike"></CounterButton>
+                        <CounterButton
+                            clickHandler={() => { this.handleLike(this.state.likes) }}
+                            buttonname="Like"
+                            numOfLikes={this.state.likes}
+                            counter={this.state.likes}
+                        />
+                        <CounterButton
+                            clickHandler={() => { this.handleLike('unlikes') }}
+                            buttonname="Unlike"
+                            numOfDislikes={this.state.dislikes}
+                            counter={this.state.dislikes}
+                        />
                     </div>
                 </div>
             </div>
